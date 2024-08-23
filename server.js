47ef1f44aa6e59a6aaf1ff7298ddd2e56f0eddcd
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-08-23 16:55:56 +0700

    server started

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const userRoutes = require('./routes/userRoutes');
 const app = express();
 
 // Connect Database
-connectDB();
+connectDB().then(r => {
+    console.log('Database connected')
+});
 
 // Init Middleware
 app.use(express.json({extended: false}));
@@ -16,3 +18,8 @@ app.use('/api/users', userRoutes);
 // Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+// Define a route for the root URL
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World</h1>');
+});
